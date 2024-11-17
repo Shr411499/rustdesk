03565e55006d708abf290137bf6cbb08d403c2c1
Shr411499 <411499292@qq.com>
2024-11-17 19:24:05 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["sr.shr4114.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "NIThATh4TD3HM+SLoEHwheeF5Gr2qS5Kfzlb6AsNIUs=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["11.shr4111.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "N112qS5Kfzlb6AsNIUs=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
